819dba14de2ae60796f0c406a31f72e3f08aef74
Chobocho <seunghwa.cho@samsung.com>
2026-04-04 12:58:18 +0900

    Add setup script for Jupyter Notebook installation
    Install Jupyter Notebook and required dependencies.

---
+++ jupyter/setup_jupyter.sh
@@ -0,0 +1,14 @@
+# Jupyter Notebook 설치
+pkg update -y && pkg upgrade -y
+
+echo "export ANDROID_API_LEVEL=$(getprop ro.build.version.sdk)" >> ~/.bashrc
+source ~/.bashrc
+
+pkg install -y python rust libffi clang make cmake git libbz2 zlib libjpeg-turbo
+pkg install -y binutils
+CFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib" pip install --no-cache-dir --force-reinstall cffi
+
+pip install --upgrade pip setuptools wheel
+pip install --no-cache-dir jupyter
+
+python add_to_bashrc.py
